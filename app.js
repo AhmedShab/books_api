63d48c04a972339fb7c86a5e204241b730ce08a6
AhmedShab <a.shaban@qetaa.com>
2019-05-10 16:08:23 +0300

    add a get by id

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const db = mongoose.connect('mongodb://localhost/bookAPI', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/bookAPI', { useNewUrlParser: true });
 const port = process.env.PORT || 3000;
 const bookRouter = express.Router();
 const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
   .get((req, res) => {
-    Book.find((error, books) => {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = query.genre;
+    }
+    Book.find(query, (error, books) => {
+      if (error) {
+        return res.send(error);
+      }
+      return res.json(books);
+    });
+  });
+
+bookRouter.route('/books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (error, books) => {
       if (error) {
         return res.send(error);
       }
