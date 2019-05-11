887f1a5328713cbed0c0d35619d55a923628c531
AhmedShab <a.shaban@qetaa.com>
2019-05-11 08:19:00 +0300

    add delete

--- routers/bookRouter.js
+++ routers/bookRouter.js
@@ -72,6 +72,14 @@ function routes(Book) {
         }
         return res.send(book);
       });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204);
+      });
     });
 
   return bookRouter;
